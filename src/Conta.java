043f61538596a5deef39c9f81d459f9c6b99d842
Atr0fy <2006lucasns@gmail.com>
2024-08-25 18:03:49 -0300

    banco

--- src/Conta.java
+++ src/Conta.java
@@ -3,24 +3,66 @@ public class Conta
     private double saldo;
     private String tipo;
 
-    public Conta(double valorInicial){
-
+    public Conta(double valorInicial)
+    {
+        this.saldo = valorInicial;
     }
     public Conta(double valorInicial, String tipo)
     {
-
+        this.saldo = valorInicial;
+        this.tipo = tipo;
     }
     public boolean depositar(double valor)
     {
-        return true;
+        if ("corrente".equals(this.tipo) && valor > 1000.00)
+        {
+            System.out.println("Valor superior a 1.000!");
+            return false;
+        }
+            this.saldo = saldo + valor;
+            return true;
     }
     public boolean sacar(double valor)
     {
-        return true;
+        if(valor > this.saldo)
+        {
+            System.out.println("Saque inválido! :/");
+            return false;
+        }
+            this.saldo = saldo - valor;
+            return true;
     }
     public boolean transferir(double valor, Conta contaDestino)
     {
+        if("poupanca".equals(this.tipo) && "corrente".equals(contaDestino.tipo))
+        {
+            System.out.println("Transferência inválida, conta poupanca para corrente!");
+            return false;
+        }
+        if(valor > this.saldo)
+        {
+            System.out.println("Saque inválido, saldo insuficiente");
+        }
+        System.out.println("Transferindo....");
+        this.saldo = saldo - valor;
+        contaDestino.saldo = saldo + valor;
         return true;
     }
+    public void exibirSaldo() {
+        System.out.println("Saldo da conta " + this.tipo + ": R$ " + this.saldo);
+    }
 
+    //nada a ver aqui
+    public static void op1(){
+        System.out.println("=========BANCO=========");
+        System.out.println("Escolher tipo de conta: ");
+        System.out.println("Corrente: 1");
+        System.out.println("Poupança: 2");
+        System.out.print("Escolha: ");
+    }
+    public static void op2(){
+        System.out.println("o que deseja fazer?");
+        System.out.println("Sacar: 1 || Depositar: 2 || Transferir: 3");
+        System.out.print("Escolha: ");
+    }
 }
