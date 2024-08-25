bc9050ae7159cae5b391c9f19c3f5887b03f8b53
Atr0fy <2006lucasns@gmail.com>
2024-08-25 18:42:03 -0300

    Orientações

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        //essa demorou para fazer ein to cansado !
+        /*
+            essa demorou para fazer ein to cansado !
+            a Classe TesteConta executa todos os testes propostos no pdf da atividade
+            A Classe DigitarTeste é a atividade com todos os valores digitados pelo usuario
+        */
     }
 }
